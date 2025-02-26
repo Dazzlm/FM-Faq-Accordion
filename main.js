6e1dc3f1c3bdfc5882cf8efc265bea30dbaae0d2
Daniel Marin <dmarin@abatechnology.com>
2025-02-26 14:51:08 -0500

    Funcionalidar de ocultar y mostrar finalizada

--- main.js
+++ main.js
@@ -1,21 +1,30 @@
 
 const sections = document.querySelectorAll('.TexBtn');
-
+let actual = "";
 sections.forEach((section) => {
   const responseId = section.getAttribute("value");
-
+  
   const button = section.querySelector('.boton');
   
   section.addEventListener('click', () => {
    
     const answer = document.getElementById("Resp"+responseId);
-    
-    if (answer.style.display === "one" || answer.style.display === "") {
+ 
+    if (answer.style.display === "none" || answer.style.display === "") {
       answer.style.display = "block";
       button.querySelector("img").src = "/JS/faq-accordion-main/assets/images/icon-minus.svg";
+      actual = responseId;
+  
     } else {
       answer.style.display = "none";
-      button.querySelector("img").src = "/JS/faq-accordion-main/assets/images/icon-plus.svg";  // Restaurar el icono cuando se contrae
+      button.querySelector("img").src = "/JS/faq-accordion-main/assets/images/icon-plus.svg"; 
+
+    }
+    for(let i = 1; i < sections.length+1 ; i++){
+      let resp = document.getElementById("Resp"+i.toString());
+      let boton = document.getElementById("Btn"+i.toString());
+      
     }
   });
 });
+
